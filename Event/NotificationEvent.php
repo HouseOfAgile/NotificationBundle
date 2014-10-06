113a2c327f09b41419dfb4184d85a829ad9e067f
jc <jc@houseofagile.com>
2014-10-06 17:25:14 +0200

    Add Generic Event, and basic template for new user as an example

---
+++ Event/NotificationEvent.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace HOA\Bundle\NotificationBundle\Event;
+
+use Symfony\Component\EventDispatcher\Event;
+
+
+class NotificationEvent extends Event implements NotificationEventInterface {
+
+    private $eventContext;
+
+    public function __construct($member) {
+        $this->eventContext = $member;
+    }
+
+    public function getEventContext() {
+        return $this->eventContext;
+    }
+
+    public function getNotificationOwner()
+    {
+        return $this->getEventContext();
+    }
+
+}
